7919b9430c59769a524a587a47b339efd25170db
catmanti <somaratne@yahoo.com>
2022-07-24 10:00:39 +0530

    Change Snippets with alter the database

---
+++ thk/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-b&hrz@^$#gu%(9_2ne9w6y#51om)tkfa_tt^q$3wq6e)tv1i5b"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
